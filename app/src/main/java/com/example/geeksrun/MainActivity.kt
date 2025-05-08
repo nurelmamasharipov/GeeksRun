859ae852d1021b75e0ff51653ac788adc2c43cf1
nurelmamasharipov <nurikmathematic@gmail.com>
2025-05-08 19:06:58 +0600

    h

--- app/src/main/java/com/example/geeksrun/MainActivity.kt
+++ app/src/main/java/com/example/geeksrun/MainActivity.kt
@@ -4,7 +4,6 @@ package com.example.geeksrun
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import com.example.geeksrun.ui.GameOverScreen
 import com.example.geeksrun.ui.GameScreen
